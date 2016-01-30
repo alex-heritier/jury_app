017ef7ed3470607ff625742735907371db0f0280
Alex Heritier <alex.heritier@gmail.com>
2016-01-30 11:45:29 -0800

    create_case.php now creates 12 votes and randomly assigns them a unique user that isn't the case creator

--- server/create_case.php
+++ server/create_case.php
@@ -2,10 +2,10 @@
 
 require 'db.php';
 
-function create_juror_id_array($mysqli)
+function create_juror_id_array($mysqli, $user_id)
 {
     $ids = array();
-    $random_user_id_query = "SELECT id FROM users ORDER BY RAND() LIMIT 12";
+    $random_user_id_query = "SELECT id FROM users WHERE id!='$user_id' ORDER BY RAND() LIMIT 12";
     $rows = $mysqli->query($random_user_id_query);
     while ($row = $rows->fetch_array(MYSQLI_NUM)) {
         array_push($ids, $row[0]);
@@ -24,7 +24,7 @@ $creator_id = $_GET['user_id'];
 //$title = "title " . rand(0, 100);
 //$description = "description " . rand(0, 100);
 //$active = 1;
-//$creator_id = rand(0, 100);
+//$creator_id = 74;
 
 $db = new DB();
 $mysqli = $db->get_mysqli();
@@ -32,17 +32,17 @@ $mysqli = $db->get_mysqli();
 //print("Creating case...\n");
 $case_create_query = "INSERT INTO cases (prosecutor, defender, title, description, active, creator_id) "
     . "VALUES ('$prosecutor', '$defender', '$title', '$description', '$active', '$creator_id')";
-//print("$case_create_query\n");
+print("$case_create_query\n");
 $mysqli->query($case_create_query);
 $case_id = $mysqli->insert_id;
 
 // create verdicts
-print("Creating verdicts...\n");
+//print("Creating verdicts...\n");
 // create the 12 different verdict slots
-$juror_ids = create_juror_id_array($mysqli);
+$juror_ids = create_juror_id_array($mysqli, $creator_id);
 for ($i = 0; $i < count($juror_ids); $i++) {
     $juror_id = $juror_ids[$i];
     $verdict_create_query = "INSERT INTO votes (juror_id, case_id) VALUES ('$juror_id', '$case_id')";
-    print($verdict_create_query);
+    //print($verdict_create_query);
     $mysqli->query($verdict_create_query);
 }
