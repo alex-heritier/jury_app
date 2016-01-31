2f85edc69b596df1b5029b4eaa822bd5765d9c42
Alex Heritier <alex.heritier@gmail.com>
2016-01-30 20:54:32 -0800

    trying to fix master

--- server/create_case.php
+++ server/create_case.php
@@ -13,12 +13,15 @@ function create_juror_id_array($mysqli, $user_id)
     return $ids;
 }
 
-$prosecutor = $_GET['prosecutor'];
-$defender = $_GET['defender'];
-$title = $_GET['title'];
-$description = $_GET['description'];
+$db = new DB();
+$mysqli = $db->get_mysqli();
+
+$prosecutor = $mysqli->real_escape_string($_GET['prosecutor']);
+$defender = $mysqli->real_escape_string($_GET['defender']);
+$title = $mysqli->real_escape_string($_GET['title']);
+$description = $mysqli->real_escape_string($_GET['description']);
 $active = 1;
-$creator_id = $_GET['user_id'];
+$creator_id = $mysqli->real_escape_string($_GET['user_id']);
 //$prosecutor = "prosecutor " . rand(0, 100);
 //$defender = "defender " . rand(0, 100);
 //$title = "title " . rand(0, 100);
@@ -26,23 +29,34 @@ $creator_id = $_GET['user_id'];
 //$active = 1;
 //$creator_id = 74;
 
-$db = new DB();
-$mysqli = $db->get_mysqli();
 // creating case
 //print("Creating case...\n");
 $case_create_query = "INSERT INTO cases (prosecutor, defender, title, description, active, creator_id) "
     . "VALUES ('$prosecutor', '$defender', '$title', '$description', '$active', '$creator_id')";
-print("$case_create_query\n");
+//print("$case_create_query\n");
 $mysqli->query($case_create_query);
 $case_id = $mysqli->insert_id;
 
 // create verdicts
 //print("Creating verdicts...\n");
-// create the 12 different verdict slots
 $juror_ids = create_juror_id_array($mysqli, $creator_id);
+$vote_ids = array();
+// create the 12 different verdict slots
 for ($i = 0; $i < count($juror_ids); $i++) {
     $juror_id = $juror_ids[$i];
     $verdict_create_query = "INSERT INTO votes (juror_id, case_id) VALUES ('$juror_id', '$case_id')";
     //print($verdict_create_query);
     $mysqli->query($verdict_create_query);
+    $last_vote_id = $mysqli->insert_id;
+    //print($last_vote_id . " ");
+    array_push($vote_ids, $last_vote_id);
 }
+// update the case's vote IDs to point at the new votes
+$update_vote_slot_query = "UPDATE cases SET ";
+for ($i = 1; $i < count($vote_ids)+1; $i++) {
+    $update_vote_slot_query .= "v{$i}_id='" . $vote_ids[$i-1] . "', ";
+}
+$update_vote_slot_query = substr($update_vote_slot_query, 0, strlen($update_vote_slot_query)-2);
+$update_vote_slot_query .= " WHERE id='$case_id'";
+//print($update_vote_slot_query);
+$mysqli->query($update_vote_slot_query);
