23beeecd997c2d0dc0a7513e2e7beb07740f91b7
Alex Heritier <alex.heritier@gmail.com>
2016-01-30 23:36:04 -0800

    bug fixes on cast_verdict.php and get_juror_cases.php

--- server/cast_verdict.php
+++ server/cast_verdict.php
@@ -6,8 +6,8 @@ $vote_id = $_GET['vote_id'];
 $case_id = $_GET['case_id'];
 $verdict = $_GET['verdict'];
 $comment = $_GET['comment'];
-//$vote_id = 433;
-//$case_id = 112;
+//$vote_id = 542;
+//$case_id = 122;
 //$verdict = rand(0, 100);
 //$comment = "An employee gave me head in the bathroom.";
 
@@ -19,24 +19,22 @@ $vote_update_query = "UPDATE votes SET verdict='$verdict', explanation='$comment
 //print($vote_update_query);
 $mysqli->query($vote_update_query);
 
-// check if all of case's votes haves verdicts
-$check_case_query = "SELECT id FROM cases WHERE id='$case_id' AND v1_id!='NULL'"
-    . " AND v2_id!='NULL'"
-    . " AND v3_id!='NULL'"
-    . " AND v4_id!='NULL'"
-    . " AND v5_id!='NULL'"
-    . " AND v6_id!='NULL'"
-    . " AND v7_id!='NULL'"
-    . " AND v8_id!='NULL'"
-    . " AND v9_id!='NULL'"
-    . " AND v10_id!='NULL'"
-    . " AND v11_id!='NULL'"
-    . " AND v12_id!='NULL'";
-//print($check_case_query);
-$result = $mysqli->query($check_case_query);
-if ($result->num_rows != 0) {
+// increment case's vote count
+$get_vote_count_query = "SELECT vote_count FROM cases WHERE id='$case_id'";
+$vote_count = $mysqli->query($get_vote_count_query)->fetch_array(MYSQLI_NUM)[0];
+$vote_count++;
+print($vote_count);
+if ($vote_count >= 12) { // if vote_count is at 12 (the max)
     // set case to inactive
-    $deactivate_case_query = "UPDATE cases SET active=0 WHERE id='$case_id'";
-    print($deactivate_case_query);
+    $deactivate_case_query = "UPDATE cases SET active=0, vote_count='$vote_count' WHERE id='$case_id'";
+    //print($deactivate_case_query);
     $mysqli->query($deactivate_case_query);
+    // set associated votes to inactive
+    $deactivate_votes_query = "UPDATE votes SET active=0 WHERE case_id='$case_id'";
+    print($deactivate_votes_query);
+    $mysqli->query($deactivate_votes_query);
+} else {
+    // increment vote_count
+    $increment_vote_count_query = "UPDATE cases SET vote_count='$vote_count' WHERE id='$case_id'";
+    $mysqli->query($increment_vote_count_query);
 }
